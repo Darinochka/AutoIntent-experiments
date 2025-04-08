c58933dfbf2ca055fc5e797af5ee02b5a7bbd3b9
Ilia Alekseev <alekseev@dgx1.ipavlov.mipt.ru>
2025-04-08 20:24:25 +0300

    update wandb_loading script

--- wandb_loading.py
+++ wandb_loading.py
@@ -1,6 +1,4 @@
 if __name__ == "__main__":
-    # script to get all wandb run data, built from their api examples
-    import pandas as pd
     import wandb
     from pathlib import Path
     from argparse import ArgumentParser
@@ -18,7 +16,6 @@ if __name__ == "__main__":
         if a_project.name not in args.projects:
             continue
         project_savedir = savedir / a_project.name
-        run_list, config_list, name_list = [], [], []
         runs = api.runs(f"ilya_alekseev_2016/{a_project.name}")
         for i, run in enumerate(runs):
             if run.name != "final_metrics":
@@ -26,22 +23,7 @@ if __name__ == "__main__":
             
             print(f"Processing run {run.name} in group {run.group} from experiment {a_project.name}")
 
-            run_list.append(run.summary._json_dict)
-
-            config_list.append(
-                {k: v for k, v in run.config.items() if not k.startswith("_")}
-            )
-            name_list.append(run.name)
-
             run_save_path = project_savedir / str(run.group)
             run_save_path.mkdir(parents=True, exist_ok=True)
             final_config = next(file for file in run.files() if Path(file.name).name == "config.yaml")
             final_config.download(root=run_save_path)
-
-        runs_df = pd.DataFrame(
-            {"summary": run_list, "config": config_list, "name": name_list}
-        )
-
-        csv_savedir = project_savedir / "csv"
-        csv_savedir.mkdir(parents=True)
-        runs_df.to_csv(csv_savedir / f"{a_project.name}.csv")
