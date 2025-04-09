f3f8951ed740c7e03bf2d1e585c573f2f9ae6572
Ilia Alekseev <alekseev@dgx1.ipavlov.mipt.ru>
2025-04-09 12:36:14 +0300

    update ho vs cv script

--- quality_ho_cv.py
+++ quality_ho_cv.py
@@ -6,41 +6,6 @@ from autointent import Dataset, Pipeline
 from autointent.configs import LoggingConfig, EmbedderConfig, DataConfig
 
 
-# search_space_raw = """
-# - node_type: scoring
-#   target_metric: scoring_accuracy
-#   metrics: [scoring_roc_auc, scoring_precision, scoring_recall, scoring_f1]
-#   search_space:
-#     - module_name: knn
-#       k:
-#         low: 1
-#         high: 20
-#       weights: [uniform, distance, closest]
-#     - module_name: linear
-#     - module_name: dnnc
-#       k:
-#         low: 1
-#         high: 5
-#     - module_name: rerank
-#       k: [30]
-#       weights: [uniform]
-#       m: [ 1, 2, 3, 4, 5 ]
-#     - module_name: sklearn
-#       clf_name: [RandomForestClassifier]
-#       n_estimators: [50, 100, 150]
-#       max_depth: [10, 30, 60]
-#       max_features: [sqrt, log2, null]
-# - node_type: decision
-#   target_metric: decision_accuracy
-#   search_space:
-#     - module_name: threshold
-#       thresh:
-#         low: 0.1
-#         high: 0.9
-#         step: 0.1
-#     - module_name: argmax
-# """
-
 search_space_raw = """
 - node_type: scoring
   target_metric: scoring_accuracy
@@ -49,9 +14,22 @@ search_space_raw = """
     - module_name: knn
       k:
         low: 1
-        high: 2
-      weights: [uniform]
+        high: 20
+      weights: [uniform, distance, closest]
     - module_name: linear
+    - module_name: dnnc
+      k:
+        low: 1
+        high: 5
+    - module_name: rerank
+      k: [30]
+      weights: [uniform]
+      m: [ 1, 2, 3, 4, 5 ]
+    - module_name: sklearn
+      clf_name: [RandomForestClassifier]
+      n_estimators: [50, 100, 150]
+      max_depth: [10, 30, 60]
+      max_features: [sqrt, log2, null]
 - node_type: decision
   target_metric: decision_accuracy
   search_space:
@@ -62,6 +40,28 @@ search_space_raw = """
         step: 0.1
     - module_name: argmax
 """
+
+# search_space_raw = """
+# - node_type: scoring
+#   target_metric: scoring_accuracy
+#   metrics: [scoring_roc_auc, scoring_precision, scoring_recall, scoring_f1]
+#   search_space:
+#     - module_name: knn
+#       k:
+#         low: 1
+#         high: 2
+#       weights: [uniform]
+#     - module_name: linear
+# - node_type: decision
+#   target_metric: decision_accuracy
+#   search_space:
+#     - module_name: threshold
+#       thresh:
+#         low: 0.1
+#         high: 0.9
+#         step: 0.1
+#     - module_name: argmax
+# """
 
 datasets_names = ["DeepPavlov/banking77", "DeepPavlov/minds14", "DeepPavlov/hwu64", "DeepPavlov/snips", "DeepPavlov/massive"]
 
