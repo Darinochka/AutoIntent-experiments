21f4c6b95d16d55b4b3790496075be6342fc24f2
voorhs <ilya_alekseev_2016@list.ru>
2025-04-15 22:53:40 +0300

    calc aggregated metrics for crossvalidation and holdout experiments

--- prune_final_configs.py
+++ prune_final_configs.py
@@ -3,14 +3,14 @@ if __name__ == "__main__":
     import yaml
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument("--expriment-name", type=str, required=True, help="Name of wandb project")
+    parser.add_argument("--experiment-name", type=str, required=True, help="Name of wandb project")
     parser.add_argument("--configs-dir", type=str, default="wandb_downloads")
     parser.add_argument("--output-dir", type=str, default="scoring_results")
     args = parser.parse_args()
 
     configs_dir = Path(args.configs_dir) / args.experiment_name
     output_dir = Path(args.output_dir) / args.experiment_name
-    output_dir.mkdir(exist_ok=True)
+    output_dir.mkdir(exist_ok=True, parents=True)
 
     for dataset_dir in configs_dir.iterdir():
         config_file = dataset_dir / "config.yaml"
