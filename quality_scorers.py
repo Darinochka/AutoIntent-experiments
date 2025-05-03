93b6571d9d76b51edb287a65e826e88130c4d3e2
voorhs <ilya_alekseev_2016@list.ru>
2025-05-04 01:20:59 +0300

    upd dependencies

--- quality_scorers.py
+++ quality_scorers.py
@@ -27,9 +27,29 @@ search_space_raw = """
       m: [ 1, 2, 3, 4, 5 ]
     - module_name: sklearn
       clf_name: [RandomForestClassifier]
-      n_estimators: [50, 100, 150]
-      max_depth: [10, 30, 60]
+      n_estimators: [200, 300, 500]
+      max_depth: [100, 150, 200]
       max_features: [sqrt, log2, null]
+    - module_name: bert
+      num_train_epochs: [3]
+      batch_size: [8, 16, 32]
+      learning_rate: [5.0e-5, 1.0e-4, 5.0e-4, 1.0e-3]
+    - module_name: lora
+      num_train_epochs: [3]
+      batch_size: [8, 16, 32]
+      learning_rate: [5.0e-5, 1.0e-4, 5.0e-4, 1.0e-3]
+      lora_alpha: [16, 32, 64]
+      lora_dropout: [0.1, 0.2, 0.3]
+      r: [8, 16, 32, 64]
+    - module_name: ptuning
+      num_train_epochs: [3]
+      batch_size: [8, 16, 32]
+      learning_rate: [5.0e-5, 1.0e-4, 5.0e-4, 1.0e-3]
+      num_virtual_tokens: [10, 30, 50]
+      encoder_dropout: [0.1, 0.2, 0.3]
+      encoder_hidden_size: [128, 256, 512]
+      encoder_reparameterization_type: [MLP, LSTM]
+      encoder_num_layers: [1, 2, 3]
 - node_type: decision
   target_metric: decision_accuracy
   search_space:
@@ -77,6 +97,7 @@ if __name__ == "__main__":
     parser.add_argument("--seeds", nargs="+")
     parser.add_argument("--validation-scheme", type=str, choices=["ho", "cv"])
     parser.add_argument("--cuda", type=str, default="0")
+    parser.add_argument("--n-shots", type=int, default=10)
     args = parser.parse_args()
 
     load_dotenv()
@@ -92,7 +113,7 @@ if __name__ == "__main__":
 
     for seed in args.seeds:
       for dataset in datasets_names:
-          data_config = DataConfig(scheme=args.validation_scheme)
+          data_config = DataConfig(scheme=args.validation_scheme, is_few_shot_train=True, examples_per_intent=args.n_shots)
 
           logging_config = LoggingConfig(
               run_name=dataset.split("/")[1] + f"[{seed=}]",
@@ -103,14 +124,14 @@ if __name__ == "__main__":
           )
           
           if args.embedder_name is None:
-              embedder_config = EmbedderConfig()
+              embedder_config = EmbedderConfig(use_cache=True)
           else:
-              embedder_config = EmbedderConfig(model_name=args.embedder_name)
+              embedder_config = EmbedderConfig(model_name=args.embedder_name, use_cache=True)
 
           pipe = Pipeline.from_search_space(search_space, seed=int(seed))
           pipe.set_config(logging_config)
           pipe.set_config(embedder_config)
           pipe.set_config(data_config)
 
-          pipe.fit(Dataset.from_hub(dataset), incompatible_search_space="filter")
+          pipe.fit(Dataset.from_hub(dataset), refit_after=True, incompatible_search_space="filter")
           shutil.rmtree(logging_config.dirpath)
