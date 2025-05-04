45e9e69d135d14676d335a36fe58e8ce9c7c63fa
Ilia Alekseev <alekseev@dgx1.ipavlov.mipt.ru>
2025-05-04 09:54:26 +0300

    upd sep exps

--- quality_separation.py
+++ quality_separation.py
@@ -26,14 +26,39 @@ search_space_raw = """
       weights: [uniform]
       m: [ 1, 2, 3, 4, 5 ]
     - module_name: sklearn
+      n_trials: 20
       clf_name: [RandomForestClassifier]
-      n_estimators: [100, 150, 200, 300]
-      max_depth: [30, 60, 100]
+      n_estimators: [200, 300, 500]
+      max_depth: [100, 150, 200]
       max_features: [sqrt, log2, null]
+    - module_name: bert
+      n_trials: 15
+      num_train_epochs: [3]
+      batch_size: [8, 16, 32]
+      learning_rate: [5.0e-5, 1.0e-4, 5.0e-4, 1.0e-3]
+    - module_name: lora
+      n_trials: 20
+      num_train_epochs: [3]
+      batch_size: [8, 16, 32]
+      learning_rate: [5.0e-5, 1.0e-4, 5.0e-4, 1.0e-3]
+      lora_alpha: [16, 32, 64]
+      lora_dropout: [0.1, 0.2, 0.3]
+      r: [8, 16, 32, 64]
+    - module_name: ptuning
+      n_trials: 20
+      num_train_epochs: [3]
+      batch_size: [8, 16, 32]
+      learning_rate: [5.0e-5, 1.0e-4, 5.0e-4, 1.0e-3]
+      num_virtual_tokens: [10, 30, 50]
+      encoder_dropout: [0.1, 0.2, 0.3]
+      encoder_hidden_size: [128, 256, 512]
+      encoder_reparameterization_type: [MLP, LSTM]
+      encoder_num_layers: [1, 2, 3]
 - node_type: decision
   target_metric: decision_accuracy
   search_space:
     - module_name: threshold
+      n_trials: 20
       thresh:
         low: 0.1
         high: 0.9
@@ -76,6 +101,7 @@ if __name__ == "__main__":
     parser.add_argument("--embedder-name", type=str, default=None, help="Name of HF repository. Omit this param to use AutoIntent's default embedder.")
     parser.add_argument("--seeds", nargs="+")
     parser.add_argument("--cuda", type=str, default="0")
+    parser.add_argument("--n-shots", type=int, default=10)
     args = parser.parse_args()
 
     load_dotenv()
@@ -92,7 +118,7 @@ if __name__ == "__main__":
     for seed in args.seeds:
         for ratio in [None, 0.2, 0.5, 0.7]:
             for dataset in datasets_names:
-                data_config = DataConfig(scheme="ho", separation_ratio=ratio)
+                data_config = DataConfig(scheme="ho", separation_ratio=ratio, examples_per_intent=args.n_shots, is_few_shot_train=True)
 
                 logging_config = LoggingConfig(
                     run_name=dataset.split("/")[1] + f"[{seed=}]" + f"[{ratio=}]",
@@ -112,5 +138,5 @@ if __name__ == "__main__":
                 pipe.set_config(embedder_config)
                 pipe.set_config(data_config)
 
-                pipe.fit(Dataset.from_hub(dataset), refit_after=True, incompatible_search_space="filter")
+                pipe.fit(Dataset.from_hub(dataset), refit_after=True, sampler="tpe", incompatible_search_space="filter")
                 shutil.rmtree(logging_config.dirpath)
