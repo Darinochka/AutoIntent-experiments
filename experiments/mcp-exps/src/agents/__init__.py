86e09a650bef05d8f6202945846441265a8009f4
voorhs <ilya_alekseev_2016@list.ru>
2026-02-14 16:09:01 +0300

    upd `agents.__init__`

--- experiments/mcp-exps/src/agents/__init__.py
+++ experiments/mcp-exps/src/agents/__init__.py
@@ -1,5 +1,6 @@
 """Collection of various agents."""
 
 from ._basic import create_basic_agent
+from ._tool_suggest import AgentState, create_tool_suggest_agent
 
-__all__ = ["create_basic_agent"]
+__all__ = ["AgentState", "create_basic_agent", "create_tool_suggest_agent"]
